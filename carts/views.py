63d681e60e96fb174f3ca4f26ce7b922dfb7d05f
Miku Acharya <mikuacharya3@gmail.com>
2024-09-19 19:33:27 +0530

    updated some code

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from store.models import Product,Variation
 from .models import Cart,CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 def _cart_id(request):
     cart=request.session.session_key
@@ -106,3 +107,25 @@ def remove_cart_item(request,product_id,cart_item_id):
     cart_item=CartItem.objects.get(cart=cart,product=product,id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
+@login_required(login_url='login')
+def checkout(request,total=0,quantity=0,cart_items=None):
+    try:
+        tax=0
+        grand_total=0
+        cart=Cart.objects.get(cart_id=_cart_id(request))
+        cart_items=CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total+=(cart_item.product.price*cart_item.quantity)
+            quantity+=cart_item.quantity
+        tax=(2*total)/100
+        grand_total=total+tax
+    except ObjectDoesNotExist:
+        pass
+    context={
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total
+    }
+    return render(request,'store/checkout.html',context)
